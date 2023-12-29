47a9464e1c07efc0755180e27c0a94baf5ad4f27
AdarshAIR1 <jhaadarsh234@gmail.com>
2023-12-29 17:38:13 +0530

    added download logic in fetchData.js

--- server/src/routes/fetchData.js
+++ server/src/routes/fetchData.js
@@ -1,21 +1,41 @@
-import ytpl from 'ytpl';
+import ytpl from "ytpl";
+import ytdl from "ytdl-core";
+import fs from "fs";
 
 export default async function fetchData(req, res) {
-    try {
-        const { url: playlistUrl } = req.body;
 
-        if (!playlistUrl) {
-            return res.status(400).json({ error: 'Missing playlistUrl in the request body' });
-        }
+    const playlistID = 'https://www.youtube.com/playlist?list=PLu0W_9lII9agpFUAlPFe_VNSlXW5uE0YL';
 
-        const playlistInfo = await ytpl(playlistUrl);
+    // Fetch kruchi playlist details
 
-        res.status(200).json({
-            playlistName: playlistInfo.title,
-            thumbnailLink: playlistInfo.bestThumbnail.url,
+    ytpl(playlistID, { limit: Infinity })
+        .then(playlistInfo => {
+
+            // Extract kruchi video IDs from the playlist, kali map kahi thilo hence map
+
+            const videoIDs = playlistInfo.items.map(item => item.id);
+
+            // Download heba katha
+            videoIDs.forEach(videoID => {
+                const videoURL = `https://www.youtube.com/watch?v=${videoID}`;
+
+                const options = {
+                    quality: 'highest',
+                };
+
+                //  downloading start 
+
+                ytdl(videoURL, options)
+                    .pipe(fs.createWriteStream(`${videoID}.mp4`)) // File name based on video ID
+                    .on('finish', () => {
+                        console.log(`Video ${videoID} downloaded !`);
+                    })
+                    .on('error', (err) => {
+                        console.error(`Error downloading video ${videoID}:`, err);
+                    });
+            });
+        })
+        .catch(err => {
+            console.error('Error fetching playlist:', err);
         });
-    } catch (error) {
-        console.error('Error:', error.message);
-        res.status(500).json({ error: 'Failed to fetch playlist details' });
-    }
 }
